7f2330924be2adbe6b8c549251cfb5451ac10993
Natarajan <raj.a.natarajan@accenture.com>
2017-05-15 11:17:16 +0530

    Updated GUI

--- demo/src/com/google/maps/android/utils/demo/MaintenanceFragment.java
+++ demo/src/com/google/maps/android/utils/demo/MaintenanceFragment.java
@@ -1,6 +1,7 @@
 package com.google.maps.android.utils.demo;
 
 
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,6 +11,7 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
+ *
  */
 public class MaintenanceFragment extends Fragment {
 
