62f18da65ed612c2d0b5bb9acbe20de82c97f8ed
Natarajan <raj.a.natarajan@accenture.com>
2017-05-16 18:09:56 +0530

    Corrected Pit Survey functionalities

--- demo/src/com/google/maps/android/utils/demo/PitSurveyActivity.java
+++ demo/src/com/google/maps/android/utils/demo/PitSurveyActivity.java
@@ -3,6 +3,8 @@ package com.google.maps.android.utils.demo;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -14,6 +16,8 @@ import com.google.maps.android.data.geojson.GeoJsonLayer;
 import org.json.JSONException;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -35,7 +39,7 @@ public class PitSurveyActivity extends BaseDemoActivity{
         EditText sizeText = (EditText) this.findViewById(R.id.structure_size_text);
         EditText materialText = (EditText) this.findViewById(R.id.structure_material_text);
         EditText occupancyText = (EditText) this.findViewById(R.id.structure_occupancy_text);
-        EditText jointtypeText = (EditText) this.findViewById(R.id.joint_type_text);
+        final EditText jointtypeText = (EditText) this.findViewById(R.id.joint_type_text);
         ownerSpinner = (Spinner)this.findViewById(R.id.owner_spinner);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item,owners);
@@ -44,10 +48,12 @@ public class PitSurveyActivity extends BaseDemoActivity{
 
         try {
             GeoJsonLayer eqpandpit_layer = new GeoJsonLayer(getMap(), R.raw.equipmentandpit, this);
-            GeoJsonLayer eqp_layer = new GeoJsonLayer(getMap(), R.raw.equipment, this);
-
-            for (GeoJsonFeature feature : eqpandpit_layer.getFeatures()) {
-                if(feature.getProperty("PIT_ID").equals(pitId)){
+            final GeoJsonLayer eqp_layer = new GeoJsonLayer(getMap(), R.raw.equipment_all, this);
+            GeoJsonLayer pit_layer = new GeoJsonLayer(getMap(), R.raw.pit, this);
+            final List<String> tmplist=new ArrayList<String>();
+            tmplist.clear();
+            for (GeoJsonFeature feature : pit_layer.getFeatures()) {
+                if(feature.getProperty("ID").equals(pitId)){
                     pitIdText.setText(pitId);
                     pitIdText.setKeyListener(null);
                     sizeText.setText(feature.getProperty("Size"));
@@ -60,10 +66,41 @@ public class PitSurveyActivity extends BaseDemoActivity{
                     {
                         ownerSpinner.setSelection(1);
                     }
-
-
+                    for (GeoJsonFeature feature_1 : eqpandpit_layer.getFeatures()) {
+                        if(feature.getProperty("ID").equals(feature_1.getProperty("ID"))){
+                            tmplist.add(feature_1.getProperty("EQUIPMENT_ID"));
+                        }
+                    }
                 }
 
+            }
+
+            if(tmplist.size()>0) {
+                eqpSpinner = (Spinner) this.findViewById(R.id.joint_id_spinner);
+                ArrayAdapter<String> adp = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, tmplist);
+                adp.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                eqpSpinner.setAdapter(adp);
+
+                eqpSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                        String joint_type_selected = "";
+                        int eqp_pos = eqpSpinner.getSelectedItemPosition();
+                        String eqp_selected = tmplist.get(eqp_pos);
+                        for (GeoJsonFeature feature : eqp_layer.getFeatures()) {
+                            if (feature.getProperty("EQUIPMENT_ID").equals(eqp_selected)) {
+                                joint_type_selected = feature.getProperty("TYPE");
+                            }
+                        }
+                        jointtypeText.setText(joint_type_selected);
+
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+
+                    }
+                });
             }
 
         } catch (IOException e) {
