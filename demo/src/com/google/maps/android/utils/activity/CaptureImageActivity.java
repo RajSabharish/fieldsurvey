7a911d4eb455ed838c1a34bf9d9e14188b236ed3
varada.vamsi <varada.vamsi@accenture.com>
2017-05-29 17:15:40 +0530

    modified camera functionality for both pit and duct

--- demo/src/com/google/maps/android/utils/activity/CaptureImageActivity.java
+++ demo/src/com/google/maps/android/utils/activity/CaptureImageActivity.java
@@ -7,16 +7,23 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.ImageView;
 
+import com.google.maps.android.utils.adapter.GridViewAdapter;
 import com.google.maps.android.utils.demo.R;
+
+import java.util.ArrayList;
 
 public class CaptureImageActivity extends Activity {
     private static final int CAMERA_REQUEST = 1888;
     private ImageView imageView;
     public static String DuctId,selectedValue;
-
+    private GridView gridView;
+    private GridViewAdapter gridAdapter;
+    ImageItem item;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +31,17 @@ public class CaptureImageActivity extends Activity {
         Intent intent = getIntent();
         DuctId = intent.getStringExtra("ductIdKey");
         selectedValue = intent.getStringExtra("selectedValue");
+        gridView = (GridView) findViewById(R.id.gridView);
+        gridAdapter = new GridViewAdapter(this, R.layout.grid_item_layout, getData());
+        gridView.setAdapter(gridAdapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                item = (ImageItem) parent.getItemAtPosition(position);
+                AlertDialog.Builder alert = new AlertDialog.Builder(CaptureImageActivity.this);
+                alert.setTitle("Delete");
+                alert.setMessage("Are you sure! Do you want to Delete ?").setPositiveButton("Yes", dialogClickListener1).setNegativeButton("No", dialogClickListener1).show();
+            }
+        });
         this.imageView = (ImageView)this.findViewById(R.id.imageView1);
         imageView.setOnClickListener(new View.OnClickListener() {
 
@@ -40,11 +58,28 @@ public class CaptureImageActivity extends Activity {
             @Override
             public void onClick(View v) {
                 AlertDialog.Builder alert = new AlertDialog.Builder(CaptureImageActivity.this);
-                alert.setTitle("Camera");
-                alert.setMessage("Images Captured! Do you want to take more photos ?").setPositiveButton("No", dialogClickListener).setNegativeButton("Yes", dialogClickListener).show();
+                alert.setTitle("Camera Upload");
+                if (selectedValue.equals("Yes"))
+                {
+                    alert.setMessage("Image Uploaded ! You may close the application");
+                    alert.setPositiveButton("OK",null);
+                    alert.show();
+                }
+                if (selectedValue.equals("No"))
+                {
+                    alert.setMessage("Images Uploaded! Do you want to see the alternate Ducts").setPositiveButton("Yes", dialogClickListener).setNegativeButton("No", dialogClickListener).show();
+                }
             }
         });
 
+    }
+    private ArrayList<ImageItem> getData() {
+        System.out.println(ItemList.imageItems.size());
+        for (int i = 0; i < ItemList.imageItems.size(); i++) {
+            ItemList.imageItem1.add(new ImageItem(ItemList.imageItems.get(i)));
+        }
+        ItemList.imageItems.clear();
+        return ItemList.imageItem1;
     }
 
     DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener()
@@ -55,9 +90,8 @@ public class CaptureImageActivity extends Activity {
             switch (which)
             {
                 case DialogInterface.BUTTON_POSITIVE:
-                    Intent i = new Intent(CaptureImageActivity.this, ShowPhotos.class);
-                    i.putExtra("ductIdKey",DuctId);
-                    i.putExtra("selectedValue",selectedValue);
+                    Intent i = new Intent(CaptureImageActivity.this, AlternateDucts.class);
+                    i.putExtra("id_key",DuctId);
                     startActivity(i);
                     break;
                 case DialogInterface.BUTTON_NEGATIVE:
@@ -66,11 +100,31 @@ public class CaptureImageActivity extends Activity {
             }
         }
     };
+    DialogInterface.OnClickListener dialogClickListener1 = new DialogInterface.OnClickListener()
+    {
+        @Override
+        public void onClick(DialogInterface dialog, int which)
+        {
+            switch (which)
+            {
+                case DialogInterface.BUTTON_POSITIVE:
+                    ItemList.imageItem1.remove(item);
+                    Intent intent = getIntent();
+                    finish();
+                    startActivity(intent);
+                    break;
+                case DialogInterface.BUTTON_NEGATIVE:
+                    break;
 
-
+            }
+        }
+    };
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Intent intent = getIntent();
+            finish();
+            startActivity(intent);
             Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
             ItemList.imageItems.add(thumbnail);
         }
--- demo/src/com/google/maps/android/utils/activity/CaptureImagePit.java
+++ demo/src/com/google/maps/android/utils/activity/CaptureImagePit.java
@@ -8,10 +8,15 @@ import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.ImageView;
 
+import com.google.maps.android.utils.adapter.GridViewAdapter;
 import com.google.maps.android.utils.demo.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by varada.vamsi on 19/5/2017.
@@ -20,14 +25,29 @@ public class CaptureImagePit extends AppCompatActivity {
     public static String pitId,pit_position,selectedValue;
     private static final int CAMERA_REQUEST = 1888;
     private ImageView imageView;
+    private GridView gridView;
+    private GridViewAdapter gridAdapter;
+    ImageItem item;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("inside the capture pit class");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_capture_image_pit);
         Bundle bundle = getIntent().getExtras();
         pitId = bundle.getString("pitid");
         pit_position = bundle.getString("pitposition");
         selectedValue = bundle.getString("selectedRadioIdText");
+        gridView = (GridView) findViewById(R.id.gridView1);
+        gridAdapter = new GridViewAdapter(this, R.layout.grid_item_layout, getData());
+        gridView.setAdapter(gridAdapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                item = (ImageItem) parent.getItemAtPosition(position);
+                AlertDialog.Builder alert = new AlertDialog.Builder(CaptureImagePit.this);
+                alert.setTitle("Delete");
+                alert.setMessage("Are you sure! Do you want to Delete ?").setPositiveButton("Yes", dialogClickListener1).setNegativeButton("No", dialogClickListener1).show();
+            }
+        });
         System.out.println(pitId+"pitId"+pit_position+"inside the CaptureImagePitclass");
         this.imageView = (ImageView)this.findViewById(R.id.imageView1);
         imageView.setOnClickListener(new View.OnClickListener() {
@@ -45,10 +65,27 @@ public class CaptureImagePit extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 AlertDialog.Builder alert = new AlertDialog.Builder(CaptureImagePit.this);
-                alert.setTitle("Camera");
-                alert.setMessage("Images Captured! Do you want to take more photos ?").setPositiveButton("No", dialogClickListener).setNegativeButton("Yes", dialogClickListener).show();
+                alert.setTitle("Camera Upload");
+                if (selectedValue.equals("Yes"))
+                {
+                    alert.setMessage("Image Uploaded ! You may close the application");
+                    alert.setPositiveButton("OK",null);
+                    alert.show();
+                }
+                if (selectedValue.equals("No"))
+                {
+                    alert.setMessage("Images Uploaded! Do you want to see the alternate pits").setPositiveButton("Yes", dialogClickListener).setNegativeButton("No", dialogClickListener).show();
+                }
             }
         });
+    }
+    private ArrayList<ImageItem> getData() {
+        System.out.println(ItemList.imageItems1.size());
+        for (int i = 0; i < ItemList.imageItems1.size(); i++) {
+            ItemList.imageItem2.add(new ImageItem(ItemList.imageItems1.get(i)));
+        }
+        ItemList.imageItems1.clear();
+        return ItemList.imageItem2;
     }
     DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener()
     {
@@ -58,11 +95,31 @@ public class CaptureImagePit extends AppCompatActivity {
             switch (which)
             {
                 case DialogInterface.BUTTON_POSITIVE:
-                    Intent i = new Intent(CaptureImagePit.this, ShowPitPhotos.class);
+                    Intent i = new Intent(CaptureImagePit.this, AlternatePits.class);
                     i.putExtra("pitId",pitId);
                     i.putExtra("pit_position",pit_position);
-                    i.putExtra("selectedValue",selectedValue);
                     startActivity(i);
+                    break;
+                case DialogInterface.BUTTON_NEGATIVE:
+                    break;
+
+            }
+        }
+    };
+    DialogInterface.OnClickListener dialogClickListener1 = new DialogInterface.OnClickListener()
+    {
+        @Override
+        public void onClick(DialogInterface dialog, int which)
+        {
+            switch (which)
+            {
+                case DialogInterface.BUTTON_POSITIVE:
+                    ItemList.imageItem2.remove(item);
+                    Intent intent = getIntent();
+                    finish();
+                    startActivity(intent);
+                    break;
+                case DialogInterface.BUTTON_NEGATIVE:
                     break;
 
             }
@@ -70,6 +127,9 @@ public class CaptureImagePit extends AppCompatActivity {
     };
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Intent intent = getIntent();
+            finish();
+            startActivity(intent);
             Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
             ItemList.imageItems1.add(thumbnail);
         }
