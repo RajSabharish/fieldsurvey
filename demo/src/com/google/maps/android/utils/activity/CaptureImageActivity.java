20fe74f9061a4edd8d7a662549c7b0f1acfe468e
Vamsi <varada.vamsi@accenture.com>
2017-06-05 10:05:41 +0530

    added the voice functionality for alternate pits

--- demo/src/com/google/maps/android/utils/activity/CaptureImageActivity.java
+++ demo/src/com/google/maps/android/utils/activity/CaptureImageActivity.java
@@ -1,27 +1,37 @@
 package com.google.maps.android.utils.activity;
 
 import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.app.Activity;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.maps.android.utils.adapter.GridViewAdapter;
 import com.google.maps.android.utils.demo.R;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class CaptureImageActivity extends Activity {
     private static final int CAMERA_REQUEST = 1888;
     private ImageView imageView;
     public static String DuctId,selectedValue;
     private GridView gridView;
+    private ImageButton btnSpeak;
+    private String Speechvalue;
+    TextToSpeech t1;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
     private GridViewAdapter gridAdapter;
     ImageItem item;
     @Override
@@ -31,6 +41,13 @@ public class CaptureImageActivity extends Activity {
         Intent intent = getIntent();
         DuctId = intent.getStringExtra("AssetId");
         selectedValue = intent.getStringExtra("selectedValue");
+        btnSpeak = (ImageButton) findViewById(R.id.btnSpeak);
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                promptSpeechInput();
+            }
+        });
         gridView = (GridView) findViewById(R.id.gridView);
         gridAdapter = new GridViewAdapter(this, R.layout.grid_item_layout, getData());
         gridView.setAdapter(gridAdapter);
@@ -59,16 +76,10 @@ public class CaptureImageActivity extends Activity {
             public void onClick(View v) {
                 AlertDialog.Builder alert = new AlertDialog.Builder(CaptureImageActivity.this);
                 alert.setTitle("Camera Upload");
-                if (selectedValue.equals("Yes"))
-                {
-                    alert.setMessage("Image Uploaded ! You may close the application");
-                    alert.setPositiveButton("OK",null);
-                    alert.show();
-                }
-                if (selectedValue.equals("No"))
-                {
-                    alert.setMessage("Images Uploaded! Do you want to see the alternate Ducts").setPositiveButton("Yes", dialogClickListener).setNegativeButton("No", dialogClickListener).show();
-                }
+                alert.setMessage("Image Uploaded ! You may close the application");
+                alert.setPositiveButton("OK",null);
+                alert.show();
+
             }
         });
 
@@ -121,12 +132,103 @@ public class CaptureImageActivity extends Activity {
     };
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
-            Intent intent = getIntent();
-            finish();
-            startActivity(intent);
-            Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
-            ItemList.imageItems.add(thumbnail);
+
+        switch (requestCode){
+            case CAMERA_REQUEST:
+                if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+                    Intent intent = getIntent();
+                    finish();
+                    startActivity(intent);
+                    Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+                    ItemList.imageItems.add(thumbnail);
+                }
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    System.out.println(result.get(0)+"text got from speech");
+                    Speechvalue = result.get(0).toString();
+                    Speack();
+                    if(Speechvalue != null && !Speechvalue.isEmpty()) {
+                        String[] words = Speechvalue.split("\\s");
+                        for(String w:words){
+                            System.out.println(words+"words");
+                            if (w.equals("alternate"))
+                            {
+                                AlertDialog.Builder alert = new AlertDialog.Builder(CaptureImageActivity.this);
+                                alert.setTitle("Camera Upload");
+                                alert.setMessage("Images Uploaded! Do you want to see the alternate Ducts").setPositiveButton("Yes", dialogClickListener).setNegativeButton("No", dialogClickListener).show();
+                            }
+                            if (w.equals("name"))
+                            {
+                                t1 = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+                                    @Override
+                                    public void onInit(int status) {
+                                        if (status != TextToSpeech.ERROR) {
+                                            t1.setLanguage(Locale.UK);
+                                            t1.speak("My name is Intellegent network field assistant Version 1.0", TextToSpeech.QUEUE_FLUSH, null);
+                                        }
+                                    }
+                                });
+                            }
+                            if (w.equals("designation"))
+                            {
+                                t1 = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+                                    @Override
+                                    public void onInit(int status) {
+                                        if (status != TextToSpeech.ERROR) {
+                                            t1.setLanguage(Locale.UK);
+                                            t1.speak("Network field engineer", TextToSpeech.QUEUE_FLUSH, null);
+                                        }
+                                    }
+                                });
+
+                            }
+                        }
+                    }
+                }
+                break;
+            }
+
         }
+
+    }
+    /**
+     * Showing google speech input dialog
+     * */
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void onPause() {
+        if (t1 != null) {
+            t1.stop();
+            t1.shutdown();
+        }
+        super.onPause();
+    }
+    public void Speack()
+    {
+        t1 = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    t1.setLanguage(Locale.UK);
+                    t1.speak("Do you want to see the alternate Ducts", TextToSpeech.QUEUE_FLUSH, null);
+                }
+            }
+        });
     }
 }
