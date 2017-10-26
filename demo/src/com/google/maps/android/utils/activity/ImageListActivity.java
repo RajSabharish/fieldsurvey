0ab9fd5f6e9bc5d1a23d5b982b2a08bc1396688e
Vamsi <varada.vamsi@accenture.com>
2017-10-26 18:39:48 +0530

    added the new feature in construction layout

--- demo/src/com/google/maps/android/utils/activity/ImageListActivity.java
+++ demo/src/com/google/maps/android/utils/activity/ImageListActivity.java
@@ -1,13 +1,26 @@
 package com.google.maps.android.utils.activity;
 
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.google.maps.android.utils.demo.R;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class ImageListActivity extends AppCompatActivity {
 
@@ -18,14 +31,94 @@ public class ImageListActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        System.out.println("inside the ImageList Activity class");
+
+        List vamsi = getFilePaths();
+        System.out.println("vamsi"+vamsi);
+        File imgFile = new  File("/storage/emulated/0/DCIM/Camera/Test.jpg");
+        System.out.println("imgFile ######## imgFile"+imgFile);
+        if(imgFile.exists()){
+            System.out.println("inside the if condition");
+            Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+            ImageView myImage = (ImageView) findViewById(R.id.imageView);
+            myImage.setImageBitmap(myBitmap);
+        }
+    }
+    public ArrayList<String> getFilePaths()
+    {
+
+
+        Uri u = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        String[] projection = {MediaStore.Images.ImageColumns.DATA};
+        Cursor c = null;
+        SortedSet<String> dirList = new TreeSet<String>();
+        ArrayList<String> resultIAV = new ArrayList<String>();
+
+        String[] directories = null;
+        if (u != null)
+        {
+            c = managedQuery(u, projection, null, null, null);
+        }
+
+        if ((c != null) && (c.moveToFirst()))
+        {
+            do
+            {
+                String tempDir = c.getString(0);
+                tempDir = tempDir.substring(0, tempDir.lastIndexOf("/"));
+                try{
+                    dirList.add(tempDir);
+                }
+                catch(Exception e)
+                {
+
+                }
             }
-        });
+            while (c.moveToNext());
+            directories = new String[dirList.size()];
+            dirList.toArray(directories);
+
+        }
+
+        for(int i=0;i<dirList.size();i++)
+        {
+            File imageDir = new File(directories[i]);
+            File[] imageList = imageDir.listFiles();
+            if(imageList == null)
+                continue;
+            for (File imagePath : imageList) {
+                try {
+
+                    if(imagePath.isDirectory())
+                    {
+                        imageList = imagePath.listFiles();
+
+                    }
+                    if ( imagePath.getName().contains(".jpg")|| imagePath.getName().contains(".JPG")
+                            || imagePath.getName().contains(".jpeg")|| imagePath.getName().contains(".JPEG")
+                            || imagePath.getName().contains(".png") || imagePath.getName().contains(".PNG")
+                            || imagePath.getName().contains(".gif") || imagePath.getName().contains(".GIF")
+                            || imagePath.getName().contains(".bmp") || imagePath.getName().contains(".BMP")
+                            )
+                    {
+
+
+
+                        String path= imagePath.getAbsolutePath();
+                        resultIAV.add(path);
+
+                    }
+                }
+                //  }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return resultIAV;
+
+
     }
 
 }
